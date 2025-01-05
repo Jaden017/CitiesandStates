221b98ce77506c765d3291485e8e0891a787a1ea
Jaden017 <jramire008@citymail.cuny.edu>
2025-01-05 13:01:59 -0500

    I was learning how to make the files so I created and deleted quite a bit. Also started with the shell and filled in the information as time went on.

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+function validateForm() {
+    const email = document.getElementById("email").value;
+    const confirmEmail = document.getElementById("confirmEmail").value;
+
+    if (email !== confirmEmail) {
+        alert("Email addresses do not match.");
+    } else {
+        alert("Form submitted successfully!");
+    }
+}
 
